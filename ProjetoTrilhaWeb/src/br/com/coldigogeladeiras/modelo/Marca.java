d5174006476c060a1d965483f8c08b398000cd51
DANILO DE MATOS SERAFIM <danilo_serafim@estudante.sesisenai.org.br>
2022-06-13 21:53:22 -0300

    Avanço no desenvolvimento da orientação técnica 09

--- ProjetoTrilhaWeb/src/br/com/coldigogeladeiras/modelo/Marca.java
+++ ProjetoTrilhaWeb/src/br/com/coldigogeladeiras/modelo/Marca.java
@@ -10,7 +10,18 @@ public class Marca implements Serializable {
 	private String nome;
 	
 	public int getId() {
-		return id
+		return id;
 	}
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
